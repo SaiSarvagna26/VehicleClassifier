b75dfc135ddec67da792213bc3cea9833dbf7a70
SaiSarvagna26 <saisarvagnagarre2643@gmail.com>
2023-07-10 21:28:06 +0530

    first commit

---
+++ VehicleClassifier.java
@@ -0,0 +1,39 @@
+package Vehicle;
+
+abstract class Vehicle
+{
+    abstract void start();
+    abstract void stop();
+}
+class Car extends Vehicle
+{
+    void start()
+    {
+        System.out.println("Car has Started");
+    }
+    void stop()
+    {
+        System.out.println("Car has Stopped");
+    }
+}
+class Motorcycle extends Vehicle
+{
+    void start()
+    {
+        System.out.println("Motorcycle has Started");
+    }
+    void stop()
+    {
+        System.out.println("Motorcycle has Stopped");
+    }
+}
+public class VehicleClassifier {
+    public static void main(String[] args) {
+        Vehicle car=new Car();
+        car.start();
+        car.stop();
+        Vehicle motorcycle =new Motorcycle();
+        motorcycle.start();
+        motorcycle.stop();
+    }
+}
